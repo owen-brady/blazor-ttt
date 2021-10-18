8d2fdfae56224aa1a4d84939cd1b8927bf6d6976
Owen Brady <owen.brady@daveramsey.com>
2021-10-18 12:00:14 -0500

    set up storyboard and IBOutletCollection for tiles

--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -1,18 +1,23 @@
 ﻿// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace iOS
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UILabel[] TileCollection { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
 }
