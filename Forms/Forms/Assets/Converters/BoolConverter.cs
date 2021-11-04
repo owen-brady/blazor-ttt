85d0ba21347a05a63eaffe4860ceef7cba22159f
Owen Brady <owen.brady@daveramsey.com>
2021-11-04 10:38:54 -0500

    add bool converter, bind label visibility to vm

---
+++ Forms/Forms/Assets/Converters/BoolConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Forms.Assets.Converters
+{
+    public class BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool) value;
+        }
+    }
+}
