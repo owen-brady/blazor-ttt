d13a6f7c51c4445713df41c730b7c227bc339e72
Owen Brady <owen.brady@daveramsey.com>
2021-11-04 10:06:00 -0500

    programatically set size of game board

--- Forms/Forms/BoardPage.xaml.cs
+++ Forms/Forms/BoardPage.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,33 @@ namespace Forms
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BoardPage : ContentPage
     {
+        public GameViewModel GameViewModel { get; set; }
+
         public BoardPage(GameViewModel gameViewModel)
         {
+            GameViewModel = gameViewModel;
+            BindingContext = GameViewModel;
+            var rowDefinitionCollection = new RowDefinitionCollection();
+            var rowDefinition = new RowDefinition {Height = GridSize()};
+            rowDefinitionCollection.Add(rowDefinition);
+            rowDefinitionCollection.Add(rowDefinition);
+            rowDefinitionCollection.Add(rowDefinition);
+
+            var columnDefinitionCollection = new ColumnDefinitionCollection();
+            var columnDefinition = new ColumnDefinition {Width = GridSize()};
+            columnDefinitionCollection.Add(columnDefinition);
+            columnDefinitionCollection.Add(columnDefinition);
+            columnDefinitionCollection.Add(columnDefinition);
+            
             InitializeComponent();
+            
+            Board.RowDefinitions = rowDefinitionCollection;
+            Board.ColumnDefinitions = columnDefinitionCollection;
+        }
+
+        private static double GridSize()
+        {
+            return (Application.Current.MainPage.Width - 50) / 3;
         }
     }
 }
